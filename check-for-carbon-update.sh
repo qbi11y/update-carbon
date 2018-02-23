fa4895c5eeedd9886d87751de91cc75a53d2314a
James Thompson <thompsoj@us.ibm.com>
2018-02-23 12:10:48 -0600

    Add help text to the .sh file

--- check-for-carbon-update.sh
+++ check-for-carbon-update.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-cd /Users/thompsoj/Documents/IBM\ Cloud/carbon-design-kit
+cd paste-your-carbon-design-kit-folder-path-here
+
+#The following code will capture the date and time of the last time
+#the script excuted and will give you the result.
+#If you see Already up to date, it hasn't changed since the last run time.
+#If you see a bunch of stuff that doesn't  make sense if you're not familiar with git, that means the library has been updated.
 date > lastUpdate.txt
 git pull | tee -a lastUpdate.txt
