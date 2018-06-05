c27c9680f3cd9898ae1273a7b87b555c04f45090
James Thompson <thompsoj@us.ibm.com>
2018-06-05 11:09:10 -0500

    added install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+folder_name="Update Carbon Design Kit"
+library_folder_name="Sketch Library File"
+cd ~/Documents
+#mkdir "$folder_name"
+cd "$folder_name"
+#mkdir "$library_folder_name"
+#git clone https://github.com/carbon-design-system/carbon-design-kit.git
+cd carbon-design-kit/Carbon\ Design\ Kit\ -?*
+cp Carbon\ Design\ Kit.sketch ../../"$library_folder_name"
+cd ../../"$library_folder_name"
+ls
