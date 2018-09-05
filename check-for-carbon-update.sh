b1f3aa9f6ceddc18c09beb7bd75301ac88d896a4
James Thompson <thompsoj@us.ibm.com>
2018-09-05 12:42:24 -0500

    updated script to append to file and display in console

--- check-for-carbon-update.sh
+++ check-for-carbon-update.sh
@@ -7,8 +7,8 @@ cd ~/Documents/Update\ Carbon\ Design\ Kit/carbon-design-kit
 #the script excuted and will give you the result.
 #If you see Already up to date, it hasn't changed since the last run time.
 #If you see a bunch of stuff that doesn't  make sense if you're not familiar with git, that means the library has been updated.
-date >> ../lastUpdate.txt
-git pull >> ../lastUpdate.txt
+date | tee -a ../lastUpdate.txt
+git pull | tee -a ../lastUpdate.txt
 
 #To ensure that Sketch can see the file even after the folder name is updated with the version we move the folder to a known location
 cp Carbon\ Design\ Kit\ -?*/Carbon\ Design\ Kit.sketch ../../
